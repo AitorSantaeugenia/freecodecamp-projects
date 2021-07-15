70cd5658efb39a04eb54d33fdee21728988c3409
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-07-15 18:52:46 +0200

    started project 0

---
+++ javascript-algorithms-and-data-structure-projects/project_1/project1.js
@@ -0,0 +1,11 @@
+function palindrome(str) {
+	//regExpr to remove characters we don't want
+	var regEx = /[^A-Za-z0-9]/g;
+
+	//starting to loop
+	for (let i = 0; i < str.length; i++) {}
+
+	return true;
+}
+
+console.log(palindrome('eye'));
