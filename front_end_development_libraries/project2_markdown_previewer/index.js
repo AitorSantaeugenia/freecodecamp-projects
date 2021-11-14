6249b0930ef3a558c88702ea2678443eb73b9f12
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-11-14 05:31:52 +0100

    Added indexjs and indexhtml with basic code and react already working

---
+++ front_end_development_libraries/project2_markdown_previewer/index.js
@@ -0,0 +1,9 @@
+// AJ
+// Check it on codepen: https://codepen.io/santaeugeniaJ/full/dyzqOEJ
+// Code in github: https://github.com/AitorSantaeugenia/freecodecamp-projects/tree/main/front_end_development_libraries/project2_markdown_previ
+
+function App() {
+	return <div>Hello</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
