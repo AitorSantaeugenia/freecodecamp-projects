93087883f1ac40d75f654ba34d812d5b2da60723
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-11-23 20:47:46 +0100

    Default index added

---
+++ front_end_development_libraries/project5_25_5_clock/index.js
@@ -0,0 +1,8 @@
+/* AJ
+Check it on codepen: https://codepen.io/santaeugeniaJ/full/wvqbbaY
+Code in github: https://github.com/AitorSantaeugenia/freecodecamp-projects/tree/main/front_end_development_libraries/project5_25_5_clock
+*/
+
+function App() {}
+
+ReactDOM.render(<App />, document.getElementById('app'));
