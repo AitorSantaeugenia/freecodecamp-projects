b7dc71d3b685d2b25510adc136ce2f9c2c9df59d
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-08-10 22:19:00 +0200

    User story 7 done

--- data_visualization/Project_3_Visualize_data_with_a_heat_map/index.js
+++ data_visualization/Project_3_Visualize_data_with_a_heat_map/index.js
@@ -16,11 +16,57 @@ let canvas = d3.select("#canvas");
 canvas.attr("width", width);
 canvas.attr("height", height);
 
-let generateScales = () => {};
+let generateScales = () => {
+  xScale = d3.scaleLinear().range([padding, width - padding]);
+  yScale = d3.scaleTime().range([padding, height - padding]);
+};
 
-let drawCells = () => {};
+let drawCells = () => {
+  canvas
+    .selectAll("rect")
+    .data(values)
+    .enter()
+    .append("rect")
+    .attr("class", "cell")
+    .attr("fill", (item) => {
+      variance = item["variance"];
+      if (variance <= -1) {
+        return "SteelBlue";
+      } else if (variance <= 0) {
+        return "LightSteelBlue";
+      } else if (variance <= 1) {
+        return "Orange";
+      } else {
+        return "Crimson";
+      }
+    })
+    .attr("data-year", (item) => {
+      return item;
+    })
+    .attr("data-month", (item) => {
+      return item;
+    })
+    .attr("data-temp", (item) => {
+      return item;
+    });
+};
 
-let drawAxes = () => {};
+let drawAxes = () => {
+  let xAxis = d3.axisBottom(xScale);
+  let yAxis = d3.axisLeft(yScale);
+
+  canvas
+    .append("g")
+    .call(xAxis)
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0," + (height - padding) + ")");
+
+  canvas
+    .append("g")
+    .call(yAxis)
+    .attr("id", "y-axis")
+    .attr("transform", "translate(" + padding + "," + "0)");
+};
 
 req.open("GET", url, true);
 req.onload = () => {
