3b46cb9ad38ef8a488b4ae4fb40c90e9849e509f
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-08-15 19:27:12 +0200

    Project5_starting_project

---
+++ data_visualization/Project_5_Visualize_data_with_a_Treemap_diagram/README.md
@@ -0,0 +1,5 @@
+### Visualize Data with a Treemap Diagram
+
+![Alt text]()
+[View On CodePen]() </br>
+[Try it on FreeCodeCamp](https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-treemap-diagram)
---
+++ data_visualization/Project_5_Visualize_data_with_a_Treemap_diagram/index.js
@@ -0,0 +1,9 @@
+let kickStarterURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
+let movieSalesURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+let videoGamesSalesURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+
+let canvas = d3.select("#canvas");
+let tooltip = d3.select("#tooltip");
