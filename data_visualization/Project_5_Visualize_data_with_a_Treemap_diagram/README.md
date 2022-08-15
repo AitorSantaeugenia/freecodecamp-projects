1c615c77fe585ae7cbb99106fa8d089db19923e6
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-08-15 21:04:54 +0200

    project5 done

--- data_visualization/Project_5_Visualize_data_with_a_Treemap_diagram/README.md
+++ data_visualization/Project_5_Visualize_data_with_a_Treemap_diagram/README.md
@@ -1,5 +1,5 @@
 ### Visualize Data with a Treemap Diagram
 
-![Alt text]()
-[View On CodePen]() </br>
+![Alt text](https://user-images.githubusercontent.com/14861253/184696218-eded7b62-3bf7-4c7b-8ad4-58d42ef92770.png)
+[View On CodePen](https://codepen.io/santaeugeniaJ/full/dymQbEM) </br>
 [Try it on FreeCodeCamp](https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-treemap-diagram)
--- data_visualization/Project_5_Visualize_data_with_a_Treemap_diagram/index.js
+++ data_visualization/Project_5_Visualize_data_with_a_Treemap_diagram/index.js
@@ -5,5 +5,150 @@ let movieSalesURL =
 let videoGamesSalesURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
 
+let movieData;
+
 let canvas = d3.select("#canvas");
 let tooltip = d3.select("#tooltip");
+let iconBar = d3.select("#iconBar");
+let iconClose = d3.select("#iconClose");
+let legend = d3.select("#legend");
+let legendText = d3.select("#legendText");
+
+let showMenu = () => {
+  iconBar.on("click", () => {
+    iconBar.attr("class", "hidden");
+    legend.attr("class", "");
+    iconClose.attr("class", "fa fa-times icon-Close");
+    legendText.attr("class", "hidden");
+  });
+
+  iconBar.on("mouseover", () => {
+    legendText.attr("class", "hidden");
+  });
+
+  iconBar.on("mouseout", () => {
+    legendText.attr("class", "");
+  });
+
+  iconClose.on("click", () => {
+    iconBar.attr("class", "fa fa-bars icon-Bars");
+    legend.attr("class", "hidden");
+    iconClose.attr("class", "hidden");
+    legendText.attr("class", "");
+  });
+};
+
+let drawTreeMap = () => {
+  let hierarchy = d3
+    .hierarchy(movieData, (node) => {
+      return node["children"];
+    })
+    .sum((node) => {
+      return node["value"];
+    })
+    .sort((node1, node2) => {
+      return node2["value"] - node1["value"];
+    });
+
+  let createTreeMap = d3.treemap().size([1000, 600]);
+  createTreeMap(hierarchy);
+
+  let movieTiles = hierarchy.leaves();
+
+  let block = canvas
+    .selectAll("g")
+    .data(movieTiles)
+    .enter()
+    .append("g")
+    .attr("transform", (movie) => {
+      return "translate(" + movie["x0"] + ", " + movie["y0"] + ")";
+    })
+    .attr("class", "movieBlock");
+
+  block
+    .append("rect")
+    .attr("class", "tile")
+    .attr("fill", (movie) => {
+      let category = movie["data"]["category"];
+      if (category === "Action") {
+        return "orange";
+      } else if (category === "Drama") {
+        return "lightgreen";
+      } else if (category === "Adventure") {
+        return "coral";
+      } else if (category === "Family") {
+        return "lightblue";
+      } else if (category === "Animation") {
+        return "pink";
+      } else if (category === "Comedy") {
+        return "khaki";
+      } else if (category === "Biography") {
+        return "tan";
+      }
+    })
+    .attr("data-name", (movie) => {
+      return movie["data"]["name"];
+    })
+    .attr("data-category", (movie) => {
+      return movie["data"]["category"];
+    })
+    .attr("data-value", (movie) => {
+      return movie["data"]["value"];
+    })
+    .attr("width", (movie) => {
+      return movie["x1"] - movie["x0"];
+    })
+    .attr("height", (movie) => {
+      return movie["y1"] - movie["y0"];
+    })
+    .attr("stroke", "white")
+    .on("mouseover", (movie) => {
+      tooltip
+        .transition()
+        .style("visibility", "visible")
+        .style("left", event.pageX - 50 + "px")
+        .style("top", event.pageY - 70 + "px")
+        .style("background", "rgba(0,0,0,0.8)")
+        .style("color", "white");
+
+      let revenue = movie["data"]["value"]
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+      tooltip.html("$ " + revenue + "<hr />" + movie["data"]["name"]);
+
+      tooltip.attr("data-value", movie["data"]["value"]);
+    })
+    .on("mouseout", (movie) => {
+      tooltip.transition().style("visibility", "hidden");
+    });
+
+  block
+    .append("text")
+    .text((movie) => {
+      return movie["data"]["name"];
+    })
+    .attr("x", 5)
+    .attr("y", 20)
+    .on("mouseover", (movie) => {
+      tooltip
+        .transition()
+        .style("visibility", "visible")
+        .style("left", event.pageX - 50 + "px")
+        .style("top", event.pageY - 70 + "px")
+        .style("background", "rgba(0,0,0,0.8)")
+        .style("color", "white");
+    });
+};
+
+//promise with d3 json and then
+d3.json(movieSalesURL).then((data, error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    movieData = data;
+    console.log(movieData);
+    drawTreeMap();
+    showMenu();
+  }
+});
